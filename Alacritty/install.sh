499ca1635032257f6613b684aa76d526ce16c15e
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-30 18:22:07 -0500

    Update install.sh

--- Alacritty/install.sh
+++ Alacritty/install.sh
@@ -4,9 +4,7 @@ printf 'Is this okay? '
 read -r okay
 if [[ $okay = y ]]; then
     curl https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Alacritty.txt >> ~/.config/alacritty/alacritty.yml
-    exit 0
 else
     echo "Copy and paste this text into your config:"
     curl https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Alacritty.txt
-    exit 0
 fi
