b9bbdb2e986e3961ad1ff50dc24905a37e27c4fa
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-30 13:02:16 -0500

    Update install.sh

--- Emacs/install.sh
+++ Emacs/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p ~/.emacs.d/themes
-wget -O ~/.emacs.d/themes/Neon-Blood-Emacs.el https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Neon-Blood-Emacs.el
+wget -O ~/.emacs.d/themes/Neon-Blood.el https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Neon-Blood.el
 echo "Append or edit this part in your Doom Emacs config:" && echo ""
-echo "(setq doom-theme 'Neon-Blood-Emacs)" && echo ""
+echo "(setq doom-theme 'Neon-Blood)" && echo ""
 echo "Append or edit this part in your Emacs config:" && echo ""
-echo "(load-theme 'Neon-Blood-Emacs t)"
+echo "(load-theme 'Neon-Blood t)"
 exit 0
