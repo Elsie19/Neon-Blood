4f51db177baa912f75994eafde7a3be0c49f4449
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-30 18:16:11 -0500

    Update Neon-Blood-Install-All.sh

--- Files/Neon-Blood-Install-All.sh
+++ Files/Neon-Blood-Install-All.sh
@@ -2,43 +2,42 @@
 echo "enter y or n for each app" && echo ""
 printf "Alacritty"
 read -r alacritty
-printf "Emacs"
-read -r emacs
-printf "Gedit"
-read -r gedit
-printf "Gnome-Terminal"
-read -r gnometerminal
-printf "Termite"
-read -r termite
-printf "Vim/NVim"
-read -r VIM
-printf "Xresources"
-read -r xresources
-
 if [[ $alacritty = y ]] ; then
     wget -qO- https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Alacritty/install.sh | bash
     exit 0
 fi
+printf "Emacs"
+read -r emacs
 if [[ $emacs = y ]] ; then
     wget -qO- https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Emacs/install.sh | bash
     exit 0
 fi
+printf "Gedit"
+read -r gedit
 if [[ $gedit = y ]] ; then
     sudo -qO- wget https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Gedit/install.sh | bash
     exit 0
 fi
+printf "Gnome-Terminal"
+read -r gnometerminal
 if [[ $gnometerminal = y ]] ; then
     wget -qO- https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Gnome-Terminal.sh | bash
     exit 0
 fi
+printf "Termite"
+read -r termite
 if [[ $termite = y ]] ; then
     wget -qO- https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Termite/install.sh | bash
     exit 0
 fi
+printf "Vim/NVim"
+read -r VIM
 if [[ $VIM = y ]] ; then
     wget -qO- https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Vim/install.sh | bash
     exit 0
 fi
+printf "Xresources"
+read -r xresources
 if [[ $xresources = y ]] ; then
     wget -qO- https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Xresources/install.sh | bash
     exit 0
