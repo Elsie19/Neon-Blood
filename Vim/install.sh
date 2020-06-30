de2b5b30578bacb9f861e42c0836f2d165d74a2e
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-30 10:10:53 -0500

    Update install.sh

--- Vim/install.sh
+++ Vim/install.sh
@@ -18,13 +18,13 @@ else
     if [ ! -d ~/.config/nvim/colors ]; then
     mkdir -p ~/.config/nvim/colors;
     cd ~/.config/nvim/colors
-        https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Vim/Neon-Blood.vim
-        printf 'Do you want to enable now? '
-        read -r yesorno
-        if [[ $yesorno = y ]]; then
-            echo "colorscheme Neon-Blood" >> ~/.config/nvim/config
-        else
-            exit 0
-        fi
-     fi
+    wget https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Vim/Neon-Blood.vim
+    printf 'Do you want to enable now? '
+    read -r yesorno
+    if [[ $yesorno = y ]]; then
+        echo "colorscheme Neon-Blood" >> ~/.config/nvim/config
+    else
+        exit 0
+    fi
+  fi
 fi
