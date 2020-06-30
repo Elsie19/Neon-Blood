73cb4347241f8e271a756701fd46352409ff0f53
Henryws <58742515+Henryws@users.noreply.github.com>
2020-06-30 18:31:09 -0500

    Update install.sh

--- Alacritty/install.sh
+++ Alacritty/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "this script will append the colors to the end of ~/.config/alacritty/alacritty.yml"
-printf 'Is this okay? '
-read -r okay
+echo 'Is this okay? '
+read okay
 if [[ $okay = y ]]; then
     curl https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Alacritty.txt >> ~/.config/alacritty/alacritty.yml
 else
