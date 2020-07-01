e6a3f51da22aca37de6e34a428fd93c72b470b07
Henryws <58742515+Henryws@users.noreply.github.com>
2020-07-01 09:11:30 -0500

    Update install.sh

--- Gedit/install.sh
+++ Gedit/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O /usr/share/gtksourceview-3.0/styles/Neon-Blood-gedit.xml https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Neon-Blood-gedit.xml
+cd /usr/share/gtksourceview-3.0/styles && wget -O Neon-Blood-gedit.xml https://raw.githubusercontent.com/Henryws/Neon-Blood/master/Files/Neon-Blood-gedit.xml
